3d6df216a88896edd80d6a4f43bd83853345c468
Butsan Vitaliy <skolkojvastut@gmail.com>
2020-01-26 11:43:37 +0200

    Initial Commit

---
+++ Cocktail DB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cocktail DB
+//
+//  Created by Vitaliy on 26.01.2020.
+//  Copyright © 2020 Vitaliy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
