313f457c8f9b4645947e89dbeec926762dc7d321
Butsan Vitaliy <skolkojvastut@gmail.com>
2020-02-04 22:42:33 +0200

    Fixed Filter Button

--- Cocktail DB/Classes/Extensions/ViewControllerExtension.swift
+++ Cocktail DB/Classes/Extensions/ViewControllerExtension.swift
@@ -10,14 +10,16 @@ import UIKit
 import MBProgressHUD
 
 extension UIViewController {
-   func showIndicator(withTitle title: String, and Description:String) {
-      let Indicator = MBProgressHUD.showAdded(to: self.view, animated: true)
-      Indicator.label.text = title
-      Indicator.isUserInteractionEnabled = false
-      Indicator.detailsLabel.text = Description
-      Indicator.show(animated: true)
-   }
-   func hideIndicator() {
-      MBProgressHUD.hide(for: self.view, animated: true)
-   }
+    
+    func showIndicator(withTitle title: String, and Description:String) {
+        let Indicator = MBProgressHUD.showAdded(to: self.view, animated: true)
+        Indicator.label.text = title
+        Indicator.isUserInteractionEnabled = false
+        Indicator.detailsLabel.text = Description
+        Indicator.show(animated: true)
+    }
+    
+    func hideIndicator() {
+        MBProgressHUD.hide(for: self.view, animated: true)
+    }
 }
